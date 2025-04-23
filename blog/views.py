9e74a45bda5c1b8a84651b356dcce2353fe95e31
firstencounter3 <needenergy27@gmail.com>
2025-04-23 15:56:13 +0700

    small refactoring, add share func

--- blog/views.py
+++ blog/views.py
@@ -1,14 +1,16 @@
 from django.core.paginator import EmptyPage, PageNotAnInteger, Paginator
-from django.shortcuts import get_object_or_404, render
-from .models import Post
-
+from django.core.mail import send_mail
 from django.views.generic import ListView
+from django.shortcuts import get_object_or_404, render
+
+from .models import Post
+from .forms import EmailPostForm
 
 
 def post_list(request):
     post_list = Post.published.all()
     paginator = Paginator(post_list, 2)
-    page_number = request.GET.get('page', 1)
+    page_number = request.GET.get("page", 1)
     try:
         posts = paginator.page(page_number)
     except PageNotAnInteger:
@@ -24,7 +26,7 @@ class PostListView(ListView):
     """
 
     queryset = Post.published.all()
-    context_object_name = 'posts'
+    context_object_name = "posts"
     paginate_by = 2
     template_name = "blog/post/list.html"
 
@@ -39,3 +41,34 @@ def post_detail(request, year, month, day, post):
         publish__day=day,
     )
     return render(request, "blog/post/detail.html", {"post": post})
+
+
+def post_share(request, post_id):
+    post = get_object_or_404(Post, id=post_id, status=Post.Status.PUBLISHED)
+    sent = False
+
+    if request.method == "POST":
+        form = EmailPostForm(request.POST)
+        if form.is_valid():
+            cd = form.cleaned_data
+            post_url = request.build_absolute_uri(post.get_absolute_url())
+            subject = (
+                f'{cd["name"]} ({cd["email"]})' f"recommends you read {post.title}"
+            )
+            message = (
+                f"Read {post.title} at {post_url}\n\n"
+                f'{cd["name"]}\'s comments: {cd["comments"]}'
+            )
+            send_mail(
+                subject=subject,
+                message=message,
+                from_email=None,
+                recipient_list=[cd["to"]],
+            )
+            sent = True
+    else:
+        form = EmailPostForm()
+
+    return render(
+        request, "blog/post/share.html", {"post": post, "form": form, "sent": sent}
+    )
